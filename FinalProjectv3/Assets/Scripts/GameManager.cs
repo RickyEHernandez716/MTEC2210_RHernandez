38b1ea1505da6cf8a534e25504f9aa17eb8f1bfe
RickyEHernandez716 <ricardo.hernandez@mail.citytech.cuny.edu>
2023-05-02 14:48:03 -0400

    WIPv1.2

--- FinalProjectv3/Assets/Scripts/GameManager.cs
+++ FinalProjectv3/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 
 public class GameManager : MonoBehaviour
@@ -12,28 +13,32 @@ public class GameManager : MonoBehaviour
 
     public GameObject startPoint; 
     public GameObject player;
-   public GameObject hazardPrefab;
-    public GameObject[] hazardArray;  
+    public GameObject hazardPrefab;
+    public GameObject[] hazardArray;
+    public GameObject camTarget;
+    public GameObject cam;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("spawnHazard1", 0, 1);
-        InvokeRepeating("spawnHazard2", 1, 1);
+        //InvokeRepeating("spawnHazard1", 0, 1);
+        //InvokeRepeating("spawnHazard2", 1, 1);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-   
+        //Uses transform.position to declare new vector 3 where position is assigned to variable 'target' on each axis except z.
+        cam.transform.position = new Vector3(cam.transform.position.x, camTarget.transform.position.y, -10);
     }
 
 private void OnCollisionEnter2D(Collision2D playerDeath)
 {
     if (playerDeath.gameObject.CompareTag("Player"))
     {
-        player.transform.position = startPoint.transform.position;
+        //player.transform.position = startPoint.transform.position;
     }
 }
 /*
